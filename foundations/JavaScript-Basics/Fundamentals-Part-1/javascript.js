4e36d57783ce08ecd1f33bdb71815ca12db96aaa
Zach Derish <zderish@gmail.com>
2023-07-26 12:56:02 -0400

    Complete variables assignment

--- foundations/JavaScript-Basics/Fundamentals-Part-1/javascript.js
+++ foundations/JavaScript-Basics/Fundamentals-Part-1/javascript.js
@@ -10,3 +10,20 @@ alert(admin);
 // part 2
 let ourPlanetName = "Earth";
 let currentUserName = "John";
+
+
+/* ASSIGNMENT */
+
+console.log(23+97);
+console.log(1+2+3+4+5+6);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+console.log(9*a);
+let b = 7*a;
+console.log(b);
+
+const max = 57;
+let actual = max - 13;
+let percentage = actual/max;
