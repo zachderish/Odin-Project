64b33dcfb7f770497c6a89c573e788f17b512a28
Zach Derish <zderish@gmail.com>
2023-07-26 12:11:18 -0400

    Complete variables tutorial

---
+++ foundations/JavaScript-Basics/Fundamentals-Part-1/javascript.js
@@ -0,0 +1,12 @@
+console.log("Hello, World")
+
+/* VARIABLES TUTORIAL*/ 
+// part 1
+let name = "John";
+let admin = name;
+
+alert(admin);
+
+// part 2
+let ourPlanetName = "Earth";
+let currentUserName = "John";
