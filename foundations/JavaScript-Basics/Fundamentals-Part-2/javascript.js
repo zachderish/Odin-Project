48ada1974d8eeb0b3d945e5bdd047fcd70cfe5ff
Zach Derish <zderish@gmail.com>
2023-08-01 08:31:13 -0400

    Complete logical operators challenge

--- foundations/JavaScript-Basics/Fundamentals-Part-2/javascript.js
+++ foundations/JavaScript-Basics/Fundamentals-Part-2/javascript.js
@@ -36,3 +36,43 @@ if(username==="Admin"){
 } else{
     alert("I don't know you");
 }
+
+
+/* Using the if..else construct, write the code which asks: ‘What is the “official” name of JavaScript?’
+
+If the visitor enters “ECMAScript”, then output “Right!”, otherwise – output: “You don’t know? ECMAScript!” */
+
+let response = prompt('What is the "official" name of Javascript?');
+
+if (response=="ECMAScript") {
+    alert("Right!");
+} else {
+    alert("You don't know? ECMAScript!");
+}
+
+/* Using if..else, write the code which gets a number via prompt and then shows in alert:
+
+1, if the value is greater than zero,
+-1, if less than zero,
+0, if equals zero.
+In this task we assume that the input is always a number. */
+
+let number = prompt("Input number");
+
+if (number > 0){
+    alert(1);
+} else if (number < 0) {
+    alert(-1);
+} else {
+    alert(0);
+}
+
+/* Rewrite if..else using multiple ternary operators '?'.
+
+For readability, it’s recommended to split the code into multiple lines. */
+
+let message = (login == "Employee") ? "Hello" : 
+    (login == "Director") ? "Greetings" :
+    (login == "") ? "No login":
+    "";
+
