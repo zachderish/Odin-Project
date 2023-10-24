6af98c080ee53cbbaaf557d5b5cee678f91c034a
Zach Derish <zderish@gmail.com>
2023-10-24 18:51:06 -0400

    Add game method

--- foundations/JavaScript-Basics/Rock-Paper-Scissors/index.js
+++ foundations/JavaScript-Basics/Rock-Paper-Scissors/index.js
@@ -12,11 +12,12 @@ function getComputerChoice() {
 
 }
 
-function playRound(computerSelection, playerSelection) {
+function playRound(computerSelection, playerSelection, playerRecord) {
     playerSelection = playerSelection.toLowerCase();
     playerSelection = playerSelection.substring(0,1).toUpperCase() + playerSelection.substring(1);
 
     if (computerSelection === playerSelection){
+        playerRecord[2]++;
         return `Tie! ${computerSelection} ties ${playerSelection}`;
     }
 
@@ -24,15 +25,38 @@ function playRound(computerSelection, playerSelection) {
     let losingScenario2 = (computerSelection === "Paper" && playerSelection === "Rock");
     let losingScenario3 = (computerSelection === "Scissors" && playerSelection === "Paper");
     if (losingScenario1 ||  losingScenario2 || losingScenario3) {
+        playerRecord[1]++;
         return `You lose! ${computerSelection} beats ${playerSelection}`; 
     }
 
     else {
+        playerRecord[0]++;
         return `You won! ${playerSelection} beats ${computerSelection}`;
     }
 
 }
 
-let playerSelection = "rock";
-let computerSelection = getComputerChoice();
-console.log(playRound(computerSelection, playerSelection))
+function game() {
+    let rounds = 5;
+    let playerRecord = [0, 0, 0];
+    for(let i = 0; i < rounds; i++) {
+        let playerSelection = prompt("Rock, paper or scissors? ");
+        let computerSelection = getComputerChoice();
+        console.log(playRound(computerSelection, playerSelection, playerRecord));
+    }
+    
+    if (playerRecord[0] > playerRecord[1]) {
+        console.log("You win!");
+    }
+    if (playerRecord[0] < playerRecord[1]) {
+        console.log("You lose!");
+    }
+    if (playerRecord[0] === playerRecord[1]) {
+        console.log("You tie!");
+    }
+
+    console.log(`Final record: ${playerRecord[0]} wins, ${playerRecord[1]} losses, ${playerRecord[2]} ties`);
+
+}
+
+game();
